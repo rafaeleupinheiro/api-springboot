4a911b5e8103caf5aec8ab2a3c71700a1e577c97
rafael <rafaeleupinheiro@gmail.com>
2019-11-05 15:51:26 -0300

    first commit

---
+++ src/main/java/br/com/controller/TestController.java
@@ -0,0 +1,23 @@
+package br.com.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.ws.rs.core.Response;
+
+@Controller
+@RequestMapping(value = "/api")
+public class TestController {
+
+  @ResponseBody
+  @RequestMapping(
+      value = "/",
+      method = RequestMethod.GET,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public Response testeAtividade() {
+    return Response.ok().build();
+  }
+}
